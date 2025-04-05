fc98cdc37c102230bbab0f2e365c67fe105ddac3
StackOverfloweds <bryangithub32@gmail.com>
2025-04-05 20:26:18 +0700

    update: add postgress cobf

--- services/db-gateway-service/src/db/postgres/postgres.go
+++ services/db-gateway-service/src/db/postgres/postgres.go
@@ -1,1 +1,24 @@
 package postgres
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+func ConnectPostgres() (*sql.DB, error) {
+	user := os.Getenv("POSTGRES_USER")
+	pass := os.Getenv("POSTGRES_PASSWORD")
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	dbname := os.Getenv("POSTGRES_DB")
+
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, pass, host, port, dbname)
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	return db, err
+}
