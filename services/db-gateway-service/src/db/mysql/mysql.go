2fbb5e659843288c32a103df3d70a7f8d526806d
StackOverfloweds <bryangithub32@gmail.com>
2025-04-05 20:24:36 +0700

    update: add config for mysql and config

--- services/db-gateway-service/src/db/mysql/mysql.go
+++ services/db-gateway-service/src/db/mysql/mysql.go
@@ -1,1 +1,25 @@
 package mysql
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+func ConnectMySQL() (*sql.DB, error) {
+
+	user := os.Getenv("MYSQL_USER")
+	pass := os.Getenv("MYSQL_PASSWORD")
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+	dbname := os.Getenv("MYSQL_DB")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, pass, host, port, dbname)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	return db, err
+}
